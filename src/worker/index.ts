0bbef55e516453d9e86aaf868bfe4b7b4a224aa6
Mstislav Zhivodkov <stevemyz@gmail.com>
2017-07-21 13:48:38 +0700

    Update deps and change worker loader (#14)

--- src/worker/index.ts
+++ src/worker/index.ts
@@ -1,10 +1,15 @@
 import { WorkerMessage } from '../types';
 import { generalize } from './generalize';
 
-declare const postMessage: (message: any, transfer?: any[]) => void;
+export interface WorkerGlobalScope {
+    onmessage: (event: { data: WorkerMessage }) => void;
+    postMessage: (message: any, transfer?: any[]) => void;
+}
 
-onmessage = (event) => {
-    const data: WorkerMessage = event.data;
-    generalize(data);
-    postMessage(data.markers);
+export default (self: WorkerGlobalScope) => {
+    self.onmessage = (event) => {
+        const data = event.data;
+        generalize(data);
+        self.postMessage(data.markers);
+    };
 };
