73f8c5710501a6667e6d495cb3891d5860e8554c
Alex Fedosov <osvodef@gmail.com>
2019-08-08 14:30:29 +0700

    add some comments

--- src/worker/minZoom.ts
+++ src/worker/minZoom.ts
@@ -11,6 +11,72 @@ const intersectionRange: Interval = { min: -Infinity, max: Infinity };
 
 const intersectionRangeLimit: Interval = { min: 0, max: Infinity };
 
+/**
+ * Вычисляет зум, на котором лейблинг-боксы bbox1 и bbox2 начинают пересекаться.
+ *
+ * BBox определяется:
+ * 1. Якорем — точкой в координатах экрана
+ * 2. Оффсетами от якоря, обозначающими границы бокса в каждую из четырёх сторон от якоря.
+ *    Оффсеты называются minX, maxX, minY и maxY.
+ *
+ * Фукнция работает следующим образом:
+ * 1. Вычисляет текущее расстояние между якорями боксов по двум осям (dx и dy).
+ * 2. Вычисляет диапазон расстояний dx и dy, внутри которых боксы будут пересекаться.
+ *    Эти вычисления производятся отдельно для двух осей. Результаты вычислений делятся
+ *    на текущие расстояния dx и dy. Таким образом, мы получаем результат в виде
+ *    множителей текущего расстояния.
+ * 3. Выполняется пересечение полученных интервалов для x и y.
+ * 4. Берётся максимальное граница интервала, из него вычисляется финальное значение зума.
+ */
+export function getIntersectionZoom(
+    bbox1: LabelBBox,
+    bbox2: LabelBBox,
+    currentZoom: number,
+): number {
+    // Вычисляем текущее расстояние между якорями боксов
+    const dx = bbox2.anchorX - bbox1.anchorX;
+    const dy = bbox2.anchorY - bbox1.anchorY;
+
+    // Вычисляем диапазон множителей dx, внутри которого боксы пересекаются вдоль оси X
+    calcIntersectionRange(
+        intersectionRangeX,
+        bbox1.minX,
+        bbox1.maxX,
+        bbox2.minX,
+        bbox2.maxX,
+        dx,
+    );
+
+    // Вычисляем диапазон множителей dy, внутри которого боксы пересекаются вдоль оси Y
+    calcIntersectionRange(
+        intersectionRangeY,
+        bbox1.minY,
+        bbox1.maxY,
+        bbox2.minY,
+        bbox2.maxY,
+        dy,
+    );
+
+    // Находим пересечение этих диапазонов
+    intersect(
+        intersectionRange,
+        intersectionRangeX,
+        intersectionRangeY,
+        intersectionRangeLimit,
+    );
+
+    // Получили пустой интервал — боксы не пересекутся никогда
+    if (intersectionRange.min >= intersectionRange.max) {
+        return -Infinity;
+    }
+
+    // Берём максимальное значение диапазона и вычисляем из него зум
+    return currentZoom + Math.log(intersectionRange.max) / Math.log(2);
+}
+
+/**
+ * Вычисляет диапазон расстояний между якорями, в котором два бокса пересекаются вдоль одной оси.
+ */
 function calcIntersectionRange(
     dest: Interval,
     min1: number,
@@ -26,6 +92,9 @@ function calcIntersectionRange(
     dest.max = max / Math.abs(d0);
 }
 
+/**
+ * Выполняет операцию пересечения трёх интервалов. Результат записывается в интервал dest.
+ */
 function intersect(
     dest: Interval,
     interval1: Interval,
@@ -34,44 +103,4 @@ function intersect(
 ): void {
     dest.min = Math.max(interval1.min, interval2.min, interval3.min);
     dest.max = Math.min(interval1.max, interval2.max, interval3.max);
-}
-
-export function getIntersectionZoom(
-    bbox1: LabelBBox,
-    bbox2: LabelBBox,
-    currentZoom: number,
-): number {
-    const dx = bbox2.anchorX - bbox1.anchorX;
-    const dy = bbox2.anchorY - bbox1.anchorY;
-
-    calcIntersectionRange(
-        intersectionRangeX,
-        bbox1.minX,
-        bbox1.maxX,
-        bbox2.minX,
-        bbox2.maxX,
-        dx,
-    );
-
-    calcIntersectionRange(
-        intersectionRangeY,
-        bbox1.minY,
-        bbox1.maxY,
-        bbox2.minY,
-        bbox2.maxY,
-        dy,
-    );
-
-    intersect(
-        intersectionRange,
-        intersectionRangeX,
-        intersectionRangeY,
-        intersectionRangeLimit,
-    );
-
-    if (intersectionRange.min >= intersectionRange.max) {
-        return -Infinity;
-    }
-
-    return currentZoom + Math.log(intersectionRange.max) / Math.log(2);
 }
